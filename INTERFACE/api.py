80da341908fca4b7e84813ef2fa80c8232d8570a
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-10 00:36:24 +0700

    Update api.py

--- INTERFACE/api.py
+++ INTERFACE/api.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ENGINE.decision_engine import generate_choices
 
 app = FastAPI()
 
+# --- CORS FIX ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"system": "KING DIAdem Decision Engine"}
+    return {"system": "KING DIADEM Decision Engine"}
 
 @app.post("/decision")
 def decision(location: str, food: str, money: str, risk: str):
