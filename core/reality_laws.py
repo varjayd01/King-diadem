dcc8a6796d43f1bd2750c1170d1bd963572f8def
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-10 01:52:49 +0700

    Create reality_laws.py

---
+++ core/reality_laws.py
@@ -0,0 +1,14 @@
+# System Reality Laws
+
+REALITY_LAWS = {
+
+"impermanence":
+"all states drift over time",
+
+"instability":
+"stable state requires maintenance",
+
+"non_centrality":
+"no single entity controls total system"
+
+}
