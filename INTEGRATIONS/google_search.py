9a81bc318c0a35327d46486fece25cf98f5b3901
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-11 05:10:48 +0700

    Create google_search.py

---
+++ INTEGRATIONS/google_search.py
@@ -0,0 +1,31 @@
+# INTEGRATIONS/google_search.py
+
+"""
+Search tool suggestions.
+
+KING DIADEM remains platform-neutral.
+"""
+
+
+def suggest_search_tools():
+
+    tools = [
+
+        {
+            "name": "Google Search",
+            "type": "search engine"
+        },
+
+        {
+            "name": "DuckDuckGo",
+            "type": "privacy search"
+        },
+
+        {
+            "name": "Bing",
+            "type": "search engine"
+        }
+
+    ]
+
+    return tools
