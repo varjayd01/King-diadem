7b175fd6f7f9563beb73334465fe4de305b2a237
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-11 05:11:24 +0700

    Create google_docs.py

---
+++ INTEGRATIONS/google_docs.py
@@ -0,0 +1,29 @@
+# INTEGRATIONS/google_docs.py
+
+"""
+Document tools suggestions.
+"""
+
+
+def suggest_document_tools():
+
+    tools = [
+
+        {
+            "name": "Google Docs",
+            "type": "document editor"
+        },
+
+        {
+            "name": "Microsoft Word",
+            "type": "document editor"
+        },
+
+        {
+            "name": "PDF Reader",
+            "type": "document viewer"
+        }
+
+    ]
+
+    return tools
