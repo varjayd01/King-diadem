0070b16858b15981b5ec1d1b612ec554037a9e52
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-15 00:01:41 +0700

    Update api_key_manager.py

--- AUTH/api_key_manager.py
+++ AUTH/api_key_manager.py
@@ -1,22 +1,7 @@
-import uuid
+from DATABASE.credit_store import get_credits,use_credit,add_credits
 
-API_KEYS = {
-    "KING-001": {"credits":10}
-}
-
-def generate_api_key():
-    key = "KD-" + str(uuid.uuid4())[:8]
-    API_KEYS[key] = {"credits":10}
-    return key
+VALID_KEYS = set()
 
 def validate_api_key(key):
-    return key in API_KEYS
 
-def use_credit(key):
-    if API_KEYS[key]["credits"] <= 0:
-        return False
-    API_KEYS[key]["credits"] -= 1
     return True
-
-def add_credit(key, amount):
-    API_KEYS[key]["credits"] += amount
