0468b33cecb292ebd22a21a8d2449dfabe4d5ee0
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-14 19:00:30 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 
 from ENGINE.decision_engine import decision_engine
+from AUTH.api_key_manager import validate_api_key,use_credit
 
 app = FastAPI(
     title="KING DIADEM",
@@ -10,7 +11,6 @@ app = FastAPI(
 )
 
 class DecisionRequest(BaseModel):
-
     location:str
     food:int
     money:int
@@ -18,14 +18,19 @@ class DecisionRequest(BaseModel):
 
 @app.get("/")
 def root():
-
     return {
         "system":"KING DIADEM",
         "status":"running"
     }
 
 @app.post("/decision")
-def decision(req:DecisionRequest):
+def decision(req:DecisionRequest, api_key:str = Header(...)):
+
+    if not validate_api_key(api_key):
+        raise HTTPException(status_code=403,detail="Invalid API KEY")
+
+    if not use_credit(api_key):
+        raise HTTPException(status_code=402,detail="No credits")
 
     result = decision_engine(
         req.location,
@@ -33,15 +38,13 @@ def decision(req:DecisionRequest):
         100.5,
         req.food,
         req.money,
-        req.risk,
-        ""
+        req.risk
     )
 
     return result
 
 
 if __name__ == "__main__":
-
     uvicorn.run(
         "server:app",
         host="0.0.0.0",
