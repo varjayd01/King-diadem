45d081b3486ccdb51840bfe1cba4b3bf30a4c503
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-14 20:04:41 +0700

    Update stripe_payment.py

--- PAYMENT/stripe_payment.py
+++ PAYMENT/stripe_payment.py
@@ -4,21 +4,25 @@ import os
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 def create_payment_session():
+
     session = stripe.checkout.Session.create(
+
         payment_method_types=["card"],
+
         line_items=[{
-            "price_data":{
-                "currency":"usd",
-                "product_data":{
-                    "name":"KING DIADEM CREDITS"
-                },
-                "unit_amount":500
-            },
-            "quantity":1
+            "price": os.getenv("STRIPE_PRICE_ID"),
+            "quantity": 1
         }],
+
         mode="payment",
+
         success_url="https://king-diadem.onrender.com/success",
-        cancel_url="https://king-diadem.onrender.com/cancel"
+        cancel_url="https://king-diadem.onrender.com/cancel",
+
+        metadata={
+            "api_key": "KING-001"
+        }
+
     )
 
     return session.url
