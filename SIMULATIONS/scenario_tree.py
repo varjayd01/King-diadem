c980491abd8aa91a1d7d3b548cfa710f65bb3ed4
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 04:29:50 +0700

    Create scenario_tree.py

---
+++ SIMULATIONS/scenario_tree.py
@@ -0,0 +1,20 @@
+def simulate_paths(problem):
+
+    return [
+
+        {
+            "path":"wait",
+            "description":"collect more information"
+        },
+
+        {
+            "path":"act",
+            "description":"take small experimental action"
+        },
+
+        {
+            "path":"reframe",
+            "description":"change perspective"
+        }
+
+    ]
