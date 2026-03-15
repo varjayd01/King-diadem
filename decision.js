7bd0ca886e7fafe260786da2dd4738c1ecb1376c
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-15 22:42:12 +0700

    Update decision.js

--- decision.js
+++ decision.js
@@ -5,25 +5,24 @@ const food=document.getElementById("food").value
 const money=document.getElementById("money").value
 const risk=document.getElementById("risk").value
 
-
-const resultBox=document.getElementById("resultBox")
-const resultText=document.getElementById("resultText")
-
-
-resultBox.style.display="block"
-
-resultText.innerHTML="Analyzing strategic state..."
-
+document.getElementById("result").innerHTML="Analyzing..."
 
 try{
 
 const res=await fetch(
+
 "https://king-diadem.onrender.com/decision",
+
 {
+
 method:"POST",
+
 headers:{
+
 "Content-Type":"application/json"
+
 },
+
 body:JSON.stringify({
 
 location:location,
@@ -32,37 +31,20 @@ money:money,
 risk:risk
 
 })
+
 })
 
 const data=await res.json()
 
+document.getElementById("result").innerHTML=
 
-let output="<b>AI Strategic Output</b><br><br>"
+"Best action: "+data.best_action
 
-output+="Best Action: "+data.best_action+"<br>"
+}catch{
 
-output+="Score: "+data.score+"<br>"
+document.getElementById("result").innerHTML=
 
-
-resultText.innerHTML=output
-
-
-let prob=Math.min(
-95,
-Math.max(
-10,
-(Number(money)/10)+(food.length*5)-(risk.length*2)
-)
-)
-
-
-document.getElementById("probBar").style.width=prob+"%"
-document.getElementById("probText").innerHTML=prob+"%"
-
-
-}catch(e){
-
-resultText.innerHTML="System error. Node reconnecting..."
+"System reconnecting..."
 
 }
 
