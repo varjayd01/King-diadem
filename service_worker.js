e16771bae85c57ba7cf09b8b8bf67c3083e03458
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 02:04:55 +0700

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,58 +1,37 @@
-const CACHE_NAME = "king-diadem-cache-v1";
+const CACHE="king-diadem-v1"
 
-const urlsToCache = [
-  "/",
-  "/ask",
-  "/system",
-  "/dashboard"
-];
+const urls=[
 
-// install
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+"/",
+"/index.html",
+"/manifest.json"
 
-// fetch
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
+]
 
-      if (response) {
-        return response;
-      }
 
-      return fetch(event.request).then((networkResponse) => {
+self.addEventListener("install",e=>{
 
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
+e.waitUntil(
 
-      });
+caches.open(CACHE)
+.then(cache=>cache.addAll(urls))
 
-    })
-  );
-});
+)
 
-// activate
-self.addEventListener("activate", (event) => {
+})
 
-  const cacheWhitelist = [CACHE_NAME];
 
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("fetch",e=>{
 
-});
+e.respondWith(
+
+caches.match(e.request)
+.then(r=>{
+
+return r || fetch(e.request)
+
+})
+
+)
+
+})
