d356734ee35388ca1a0038bd990e6fca51f1c2cb
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-15 07:11:23 +0700

    Create api_keys.py

---
+++ AUTH/api_keys.py
@@ -0,0 +1,24 @@
+import secrets
+
+from DATABASE.credit_store import add_credits
+
+
+api_keys = {}
+
+
+def create_api_key():
+
+    key = "kd_" + secrets.token_hex(16)
+
+    api_keys[key] = {
+        "credits": 100
+    }
+
+    add_credits(key, 100)
+
+    return key
+
+
+def is_valid_key(key):
+
+    return key in api_keys
