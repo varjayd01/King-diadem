28688440a7ad493bd3239134f8d468020d6cd3e2
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 04:07:14 +0700

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,11 +1,44 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "king-diadem-v1"
 
-self.skipWaiting()
+const urlsToCache = [
+
+"/static/index.html",
+"/static/style.css",
+"/static/app.js",
+"/static/system.js",
+"/static/brain.js",
+"/static/galaxy.js"
+
+]
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache=>{
+
+return cache.addAll(urlsToCache)
 
 })
 
-self.addEventListener("fetch", event => {
+)
 
-event.respondWith(fetch(event.request))
+})
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request)
+
+})
+
+)
 
 })
