d0d9916fb6bc2b9f22bf595f4e59448486045e70
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 04:30:28 +0700

    Update galaxy.js

--- static/galaxy.js
+++ static/galaxy.js
@@ -1,10 +1,26 @@
-async function loadGalaxy(){
+const canvas = document.getElementById("galaxy")
+const ctx = canvas.getContext("2d")
 
-const res = await fetch("/ai/galaxy");
-const data = await res.json();
+let nodes = [
 
-console.log("GALAXY:",data);
+{ x:300, y:200, name:"AI CORE"},
+{ x:500, y:350, name:"DECISION"},
+{ x:700, y:200, name:"SIMULATION"}
+
+]
+
+function drawNodes(){
+
+ctx.fillStyle="cyan"
+
+nodes.forEach(n=>{
+
+ctx.beginPath()
+
+ctx.arc(n.x,n.y,6,0,Math.PI*2)
+
+ctx.fill()
+
+})
 
 }
-
-loadGalaxy();
