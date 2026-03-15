ff67e890d33b0f3e2d10971277557b1ebfa9ebdb
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 00:35:20 +0700

    Update db.py

--- DATABASE/db.py
+++ DATABASE/db.py
@@ -2,5 +2,34 @@ import sqlite3
 
 DB_NAME = "king_diadem.db"
 
-def get_conn():
-    return sqlite3.connect(DB_NAME)
+def get_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
+        password TEXT,
+        balance INTEGER DEFAULT 0
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS payments(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount INTEGER,
+        status TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
