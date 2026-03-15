61cc93e636bf15ceb39b9095c9ee849a85ae4735
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-15 23:00:35 +0700

    Update decision.js

--- decision.js
+++ decision.js
@@ -5,47 +5,55 @@ const food=document.getElementById("food").value
 const money=document.getElementById("money").value
 const risk=document.getElementById("risk").value
 
-document.getElementById("result").innerHTML="Analyzing..."
+const resultText=document.getElementById("resultText")
+
+resultText.innerHTML="Analyzing strategic state..."
 
 try{
 
 const res=await fetch(
-
 "https://king-diadem.onrender.com/decision",
-
 {
-
 method:"POST",
-
 headers:{
-
 "Content-Type":"application/json"
-
 },
-
 body:JSON.stringify({
-
 location:location,
 food:food,
 money:money,
 risk:risk
-
 })
-
-})
+}
+)
 
 const data=await res.json()
 
-document.getElementById("result").innerHTML=
+resultText.innerHTML=
 
-"Best action: "+data.best_action
-
-}catch{
-
-document.getElementById("result").innerHTML=
-
-"System reconnecting..."
+"<b>AI Strategic Output</b><br><br>"+
+"Best Action: "+data.best_action+"<br>"+
+"Score: "+data.score
 
 }
+
+catch(e){
+
+resultText.innerHTML="System error. Node reconnecting..."
+
+}
+
+}
+
+async function buyAccess(){
+
+const res=await fetch(
+"https://king-diadem.onrender.com/create-checkout",
+{method:"POST"}
+)
+
+const data=await res.json()
+
+window.location=data.url
 
 }
