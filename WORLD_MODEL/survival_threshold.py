1fb5b76dccd32fa62a803a96d36796fc9e6a3624
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 09:02:47 +0700

    Create survival_threshold.py

---
+++ WORLD_MODEL/survival_threshold.py
@@ -0,0 +1,39 @@
+KING_DIADEM_SYSTEM_BELIEF = {
+
+    "choice_minimum": 1,
+
+    "survival_floor":[
+        "food",
+        "water",
+        "shelter"
+    ],
+
+    "human_centered":True,
+
+    "logic_agnostic":True,
+
+    "nature_aligned":True
+
+}
+
+
+def check_choice(options):
+
+    if options <= SYSTEM_BELIEF["choice_minimum"]:
+
+        return "generate_more_options"
+
+    return "stable"
+
+
+def survival_priority(context):
+
+    needs = SYSTEM_BELIEF["survival_floor"]
+
+    for n in needs:
+
+        if n not in context:
+
+            return n
+
+    return "stable"
