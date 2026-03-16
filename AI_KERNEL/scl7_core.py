a06ae4ea9ea21b79d802209745074dd596190e60
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 07:53:22 +0700

    Create scl7_core.py

---
+++ AI_KERNEL/scl7_core.py
@@ -0,0 +1,17 @@
+SCL7_RULES = {
+
+"human_first":True,
+
+"preserve_choice":True,
+
+"no_harm_guidance":True,
+
+"transparent_reasoning":True,
+
+"no_authority":True
+
+}
+
+def enforce_scl7():
+
+    return SCL7_RULES
