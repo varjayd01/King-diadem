720ff1c557fd382a4ad431ccee3a98fa22407851
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-24 06:27:06 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import os
-import uvicorn
+from flask import send_from_directory
 
 from app import app
 
+# ===== Serve index.html ข้างนอก =====
+@app.route('/home')
+def root_index():
+    return send_from_directory('.', 'index.html')
+
+
 if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", "10000")),
-        reload=False,
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
